e601d92f530ee77cd9c28bdb87da37b87eee7865
SilaDemirkaya <152109535+SilaDemirkaya@users.noreply.github.com>
2024-12-10 13:00:05 -0700

    Updated auth-pop-up.js
    Fixed Function error.

--- app/popups/auth-pop-up.js
+++ app/popups/auth-pop-up.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { auth, githubProvider, signInWithPopup, onAuthStateChanged } from "../_utils/firebase";
+import React from "react";
+import { auth, githubProvider, signInWithPopup } from "../_utils/firebase";
 
 const AuthPopup = ({ onLogin }) => {
   const handleLogin = async () => {
@@ -13,21 +13,19 @@ const AuthPopup = ({ onLogin }) => {
 
   return (
     <div className="fixed inset-0 bg-[#4f455e] flex justify-center items-center">
-    <div className="bg-[#f5eed5] p-10 rounded-lg shadow-2xl w-[600px] h-[400px] flex flex-col justify-between items-center">
-      <h2 className="text-6xl font-bold text-gray-900">Login Required</h2>
-      <p className="text-3xl text-gray-700 mb-4">
-        Please log in to continue using the site.
-      </p>
-      <button
-        className="px-8 py-3 bg-[#FCB761] text-white text-3xl font-semibold rounded-lg hover:bg-[#dead71] hover:text-gray-700 transition"
-        onClick={() => console.log("Login triggered")}
-      >
-        Login with GitHub
-      </button>
+      <div className="bg-[#f5eed5] p-10 rounded-lg shadow-2xl w-[600px] h-[400px] flex flex-col justify-between items-center">
+        <h2 className="text-6xl font-bold text-gray-900">Login Required</h2>
+        <p className="text-3xl text-gray-700 mb-4">
+          Please log in to continue using the site.
+        </p>
+        <button
+          className="px-8 py-3 bg-[#FCB761] text-white text-3xl font-semibold rounded-lg hover:bg-[#dead71] hover:text-gray-700 transition"
+          onClick={handleLogin}
+        >
+          Login with GitHub
+        </button>
+      </div>
     </div>
-  </div>
-  
-
   );
 };
 
