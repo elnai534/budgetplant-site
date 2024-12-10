79a7f82fd1984ef7622d5485f162c8cb24b287bb
yanganthony4 <yanganthony4@gmail.com>
2024-12-10 11:12:45 -0700

    Updated firebase.js

--- app/_utils/firebase.js
+++ app/_utils/firebase.js
@@ -3,12 +3,13 @@ import { getAuth, GithubAuthProvider, onAuthStateChanged, signInWithPopup } from
 import { getFirestore, collection, addDoc, query, where, getDocs, onSnapshot } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDmGkFmDIRnjUwXprmE8GA5x0QIQTFR900",
+  authDomain: "budget-plant-web.firebaseapp.com",
+  projectId: "budget-plant-web",
+  storageBucket: "budget-plant-web.firebasestorage.app",
+  messagingSenderId: "410472000922",
+  appId: "1:410472000922:web:5981286e3cb87f09401e23",
+  measurementId: "G-LDWR95XVES"
 };
 
 // Initialize Firebase
