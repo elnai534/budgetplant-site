1edaa23a1db71188ba39058e824ae8bb743076cd
SilaDemirkaya <152109535+SilaDemirkaya@users.noreply.github.com>
2024-12-10 18:31:46 -0700

    Update initialization-pop-up.js
    changing styling

--- app/popups/initialization-pop-up.js
+++ app/popups/initialization-pop-up.js
@@ -18,11 +18,11 @@ const InitializationPopup = ({ onSubmit }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-80">
-        <h2 className="text-lg font-bold mb-4">Welcome!</h2>
+      <div className="bg-[#f5eed5] p-6 rounded-lg shadow-lg w-80">
+        <h2 className="text-2xl text-black font-bold mb-4">Welcome!</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="name" className="block text-sm font-medium text-gray-700 mb-1">
+            <label htmlFor="name" className="block text-lg font-medium text-gray-700 mb-1">
               Your Name
             </label>
             <input
@@ -30,12 +30,12 @@ const InitializationPopup = ({ onSubmit }) => {
               type="text"
               value={name}
               onChange={(e) => setName(e.target.value)}
-              className="w-full p-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+              className="w-full p-2 border rounded-md text-black focus:outline-none focus:ring focus:ring-blue-300"
               required
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="budget" className="block text-sm font-medium text-gray-700 mb-1">
+            <label htmlFor="budget" className="block text-lg font-medium text-gray-700 mb-1">
               Monthly Budget
             </label>
             <input
@@ -43,13 +43,13 @@ const InitializationPopup = ({ onSubmit }) => {
               type="number"
               value={budget}
               onChange={(e) => setBudget(e.target.value)}
-              className="w-full p-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+              className="w-full p-2 border text-black rounded-md focus:outline-none focus:ring focus:ring-blue-300"
               required
             />
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition"
+            className="w-full bg-[#fcb761] text-white py-2 rounded-md hover:bg-[#dead71] hover:text-gray-700 font-semibold transition"
           >
             Save
           </button>
